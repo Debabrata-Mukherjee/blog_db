c7a21e41addb69c57776726aa3e49694acff63c0
Debabrata-Mukherjee <32553631+Debabrata-Mukherjee@users.noreply.github.com>
2020-04-15 03:50:44 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const user = require("./route/user");
+const mongoose = require("mongoose");
+const path = require("path")
+
+const app = express();
+
+
+
+const InitiateMongoServer = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://mukherjeedebabrata667:mukherjeedebabrata123@cluster0-vijkf.mongodb.net/blog", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("DataBase Connected!!");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+InitiateMongoServer()
+
+
+// PORT
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.render('login');
+});
+
+app.use("/user", user);
+
+
+app.listen(PORT, (req, res) => {
+  console.log(`App started on Port ${PORT}`);
+});
+
+
+
